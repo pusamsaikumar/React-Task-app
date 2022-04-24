8d5950fbbdfd8138e9cf8c2d87ba174b0cee5838
pusamsaikumar <pusamsaikumar302@gmail.com>
2022-04-24 15:49:08 +0530

    new table-data added

---
+++ table2/src/App.js
@@ -0,0 +1,18 @@
+
+import Table from './components/fetchApiData';
+import TableDataFn from './components/TableDataFn';
+import './App.css';
+import TableData from './components/TableData';
+
+function App() {
+  return (
+    <div className="App">
+    {/*<Table />
+     <TableDataFn /> */} 
+    
+     <TableData />
+    </div>
+  );
+}
+
+export default App;
